361d76dab66aedbc0c4a718f0a23561a8d8ed284
Abdallah Abuouf <abdallah.abuouf@gmail.com>
2024-03-11 14:04:41 +0200

    Update deploy_frontend.sh

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -1,5 +1,9 @@
 echo "Start deploy frontend"
 #cd /var/www
+sudo apt update 
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+sudo npm install pm2 -g
 git clone https://github.com/abuouf/uptime-kuma.git
 cd uptime-kuma
 npm run setup
