9031bba445c5712fdba7b4ae879ddb1fa3a1ef76
Abdallah Abuouf <abdallah.abuouf@gmail.com>
2024-03-11 14:14:13 +0200

    Update deploy_frontend.sh

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -6,6 +6,6 @@ sudo apt-get install -y npm
 sudo npm install pm2 -g
 git clone https://github.com/abuouf/uptime-kuma.git
 cd uptime-kuma
-npm run setup
-npm install pm2 -g && pm2 install pm2-logrotate
-pm2 start server/server.js --name uptime-kuma
+sudo npm run setup
+sudo npm install pm2 -g && pm2 install pm2-logrotate
+sudo pm2 start server/server.js --name uptime-kuma
