9615c3cac2f2e2f44fe8e61e92630b9201f83629
Abdallah Abuouf <abdallah.abuouf@gmail.com>
2024-03-11 14:05:50 +0200

    Update deploy_backend.sh

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -2,5 +2,5 @@ echo "start deploy_backend
 sudo apt update
 sudo apt install -y apache2
 cd /var/www/html/
-git clone https://github.com/abuouf/laravel.git
-php artisan migrate
+sudo git clone https://github.com/abuouf/laravel.git
+sudo php artisan migrate
